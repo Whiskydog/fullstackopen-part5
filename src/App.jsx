5b16d84c036bec87ccf6238c3cd393187f57dbe4
Whiskydog <alan.varde@gmail.com>
2023-12-23 12:06:21 -0300

    Solve exercise 5.1

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState, useEffect } from 'react';
+import Blog from './components/Blog';
+import LoginForm from './components/LoginForm';
+import blogService from './services/blogs';
+
+const App = () => {
+  const [blogs, setBlogs] = useState([]);
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    blogService.getAll().then((blogs) => setBlogs(blogs));
+  }, []);
+
+  if (!user) {
+    return (
+      <div>
+        <h2>Please log in</h2>
+        <LoginForm setUser={setUser} />
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <h2>blogs</h2>
+      <p>{user.name} logged in</p>
+      {blogs.map((blog) => (
+        <Blog key={blog.id} blog={blog} />
+      ))}
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,43 @@
+import axios from 'axios';
+import { useState } from 'react';
+
+const LoginForm = ({ setUser }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    const response = await axios.post('/api/login', { username, password });
+    setUser(response.data);
+  };
+
+  return (
+    <form onSubmit={handleLogin}>
+      <div>
+        <label>
+          username
+          <input
+            type="text"
+            name="username"
+            value={username}
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </label>
+      </div>
+      <div>
+        <label>
+          password
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </label>
+      </div>
+      <button>login</button>
+    </form>
+  );
+};
+
+export default LoginForm;
