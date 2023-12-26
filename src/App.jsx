5584f925f5531089f99fc3aa3664dbfd2478c780
Whiskydog <alan.varde@gmail.com>
2023-12-26 09:44:01 -0300

    Solve exercise 5.10

--- src/App.jsx
+++ src/App.jsx
@@ -88,9 +88,11 @@ const App = () => {
       <Toggle label="New blog" ref={createBlogFormRef}>
         <CreateBlogForm createBlog={addBlog} />
       </Toggle>
-      {blogs.map((blog) => (
-        <Blog key={blog.id} blog={blog} onLike={() => likeBlog(blog)} />
-      ))}
+      {blogs
+        .toSorted((a, b) => b.likes - a.likes)
+        .map((blog) => (
+          <Blog key={blog.id} blog={blog} onLike={() => likeBlog(blog)} />
+        ))}
     </div>
   );
 };
