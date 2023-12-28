ce6a10963ad7b609a9154714da42708adcefc393
Whiskydog <alan.varde@gmail.com>
2023-12-28 19:53:17 -0300

    Solve exercise 5.18

--- cypress/e2e/blog_app.cy.js
+++ cypress/e2e/blog_app.cy.js
@@ -1,6 +1,9 @@
+import user from '../fixtures/user.json';
+
 describe('Blog app', function () {
   beforeEach(function () {
     cy.request('POST', `${Cypress.env('BACKEND')}/testing/reset`);
+    cy.request('POST', `${Cypress.env('BACKEND')}/users`, user);
     cy.visit('');
   });
 
@@ -11,5 +14,25 @@ describe('Blog app', function () {
     cy.get('input[name=username]');
     cy.get('input[name=password]');
     cy.contains('button', 'login');
+  });
+
+  describe('Login', function () {
+    it('succeeds with correct credentials', function () {
+      cy.get('input[name=username]').type(user.username);
+      cy.get('input[name=password]').type(user.password);
+      cy.contains('button', 'login').click();
+      cy.contains(`${user.name} logged in`);
+    });
+
+    it('fails with wrong credentials', function () {
+      cy.get('input[name=username]').type(user.username);
+      cy.get('input[name=password]').type('wrongpassword');
+      cy.contains('button', 'login').click();
+      cy.contains('Wrong username or password').and(
+        'have.css',
+        'color',
+        'rgb(151, 16, 18)'
+      );
+    });
   });
 });
